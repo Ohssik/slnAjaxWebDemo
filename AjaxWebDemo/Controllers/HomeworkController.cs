32db195e342ae242740203ba2e1f27413dac6504
TingCheng <yrrek9120@gmail.com>
2023-01-08 16:02:25 +0800

    更新作業二 首頁連結

--- AjaxWebDemo/Controllers/HomeworkController.cs
+++ AjaxWebDemo/Controllers/HomeworkController.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using AjaxWebDemo.Models;
 
 namespace AjaxWebDemo.Controllers
-{
+{    
     public class HomeworkController : Controller
     {
-        public IActionResult Index()
+        private readonly DemoContext _db;
+        public HomeworkController(DemoContext db)
         {
+            _db = db;
+        }
+        public IActionResult Index(DemoContext db)
+        {    
             return View();
         }
         public IActionResult Travel()
@@ -14,6 +20,9 @@ namespace AjaxWebDemo.Controllers
         }
         public IActionResult Register()
         {
+            Member lastMember = _db.Members.OrderBy(c=>c.MemberId).Last();
+            int newId = lastMember.MemberId+1;
+            ViewData["newId"]=newId;
             return View();
         }
     }
